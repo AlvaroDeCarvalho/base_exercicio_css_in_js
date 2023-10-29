502d8e802b4aa64ee3bc0a06b75a3bfc10d7c213
AlvaroDeCarvalho <alvarodecarvalho20@gmail.com>
2023-10-29 10:41:09 -0300

    separando os types, reorganizando os styles.

--- src/components/Vaga/index.tsx
+++ src/components/Vaga/index.tsx
@@ -1,35 +1,30 @@
-import {
-  ContainerVaga,
-  VagaTitulo,
-  VagaLink,
-  ContainerVagaHover
-} from './styles'
-type Props = {
-  titulo: string
-  localizacao: string
-  nivel: string
-  modalidade: string
-  salarioMin: number
-  salarioMax: number
-  requisitos: string[]
-}
+import * as S from './styles'
+import PropsVagas from './types'
 
-const Vaga = (props: Props) => (
-  <ContainerVaga>
-    <VagaTitulo>{props.titulo}</VagaTitulo>
+const Vaga = ({
+  titulo,
+  localizacao,
+  nivel,
+  modalidade,
+  salarioMin,
+  salarioMax,
+  requisitos
+}: PropsVagas) => (
+  <S.ContainerVaga>
+    <S.VagaTitulo>{titulo}</S.VagaTitulo>
     <ul>
-      <li>Localizacao: {props.localizacao}</li>
-      <li>Senioridade: {props.nivel}</li>
-      <li>Tipo de contratacao: {props.modalidade}</li>
+      <li>Localizacao: {localizacao}</li>
+      <li>Senioridade: {nivel}</li>
+      <li>Tipo de contratacao: {modalidade}</li>
       <li>
-        Salário: {props.salarioMin} - {props.salarioMax}
+        Salário: {salarioMin} - {salarioMax}
       </li>
-      <li>Requisitos: {props.requisitos.join(', ')}</li>
+      <li>Requisitos: {requisitos.join(', ')}</li>
     </ul>
-    <ContainerVagaHover>
-      <VagaLink href="#">Ver detalhes e candidatar-se</VagaLink>
-    </ContainerVagaHover>
-  </ContainerVaga>
+    <S.ContainerVagaHover>
+      <S.VagaLink href="#">Ver detalhes e candidatar-se</S.VagaLink>
+    </S.ContainerVagaHover>
+  </S.ContainerVaga>
 )
 
 export default Vaga
---
+++ src/components/Vaga/types.ts
@@ -0,0 +1,10 @@
+type PropsVagas = {
+  titulo: string
+  localizacao: string
+  nivel: string
+  modalidade: string
+  salarioMin: number
+  salarioMax: number
+  requisitos: string[]
+}
+export default PropsVagas
